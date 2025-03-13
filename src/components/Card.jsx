ce9820de68410b8889df59eb28163d2d39dcc4a7
OhMyLaia <ohmylaia@live.com>
2025-03-13 11:41:09 +0100

    starting app

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.cjx';
+// import { tutorialData } from 
+
+export function Card() {
+    const currentData = tutorialData[step];
+
+    return (
+        <div>Card</div>
+    )
+}
+
+export default Card
